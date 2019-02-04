d5776c9d597cb457be5c9a1ac61d2e1d976f7838
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2019-02-04 12:42:59 +0300

    ci(ci): try add path for plugin registering
    https://github.com/danielfrg/pelican-ipynb/issues/74

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,19 +13,10 @@ DEFAULT_DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-DEFAULT_PAGINATION = 10
-
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-PLUGIN_PATHS = ['pelican-plugins']
+PLUGIN_PATHS = ['./pelican-plugins']
 PLUGINS = [
     'pelican-open_graph'
 ]
